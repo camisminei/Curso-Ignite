020588dbf88eda543df578d903b5a6d905614e32
Camila <camilaminei.ifms@gmail.com>
2021-11-10 11:41:41 -0400

    config do typescript

--- reactjs/01-github-explorer/babel.config.js
+++ reactjs/01-github-explorer/babel.config.js
@@ -1,7 +1,8 @@
 module.exports = {
     presets: [
         '@babel/preset-env',
-        ['@babel/preset-react' , {
+        '@babel/preset-typescript',
+        ['@babel/preset-react', {
             runtime: 'automatic'
             // com o runtime automatic não será necessário colocar no index.jsx 
             // import React from 'react';
